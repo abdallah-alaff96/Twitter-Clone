d7c04cc67f5c73588afcf96c00206f1d36b13db5
abdallah-alaff96 <64463982+abdallah-alaff96@users.noreply.github.com>
2021-10-21 23:35:39 +0300

    local storage demo

--- Tscript.js
+++ Tscript.js
@@ -3,16 +3,19 @@ const tweetButton = document.querySelector('.tweetBox__tweetButton');
 const postParent = document.querySelector('.post_parent');
 const textInput = document.getElementById('text_input');
 const postParagraph = document.querySelector('.post-paragraph');
-let retweetBtns = document.querySelectorAll('.repeat-icon');
+const resetBtn = document.querySelector('.reset-btn');
 let idNameCounter = 2;
-// footer-DOM
-const loveIcon = document.querySelectorAll('.love-icon');
-const shareIcon = document.querySelectorAll('.share-icon');
-const repeatIcon = document.querySelectorAll('.repeat-icon');
+
+// for static HTML Posts
+// const retweetBtns = document.querySelectorAll('.repeat-icon');
+// const loveIcon = document.querySelectorAll('.love-icon');
+// const shareIcon = document.querySelectorAll('.share-icon');
+// const repeatIcon = document.querySelectorAll('.repeat-icon');
+
 // array for file storage
 let tweets = [];
 
-const createTweet = function (tweet, authorName) {
+const mainTweetFunction = function (tweet, authorName) {
   const newPost = document.createElement('div');
   newPost.classList.add('post');
   newPost.style.backgroundColor = '#e6ecf0';
@@ -46,17 +49,15 @@ const createTweet = function (tweet, authorName) {
   newPost.style.backgroundColor = 'white';
   newPost.style.transition = '2s';
 
-  let tweetObject = {
-    tweetText: tweet,
-    tweetAuthor: authorName,
-    datasetName: idNameCounter,
-  };
-  tweets.push(tweetObject);
-  console.log(tweets);
-  setLocalStorage();
+  // footer-DOM
+  const loveIcon = document.querySelector('.love-icon');
+  const shareIcon = document.querySelector('.share-icon');
+  const repeatIcon = document.querySelector('.repeat-icon');
+  footerToggle(loveIcon);
+  footerToggle(shareIcon);
+  footerToggle(repeatIcon);
 
   const retweetBtn = document.querySelector('.repeat-icon');
-  console.log(retweetBtn);
   retweetBtn.addEventListener('click', e => {
     const closestParent = retweetBtn.closest('.post');
     const tweetParagraph = closestParent.querySelector('.post-paragraph');
@@ -65,44 +66,75 @@ const createTweet = function (tweet, authorName) {
   });
 };
 
+// createTweet
+const createTweet = function (tweet, authorName) {
+  mainTweetFunction(tweet, authorName);
+  let tweetObject = {
+    tweetText: tweet,
+    tweetAuthor: authorName,
+    datasetName: idNameCounter,
+  };
+  tweets.push(tweetObject);
+  setLocalStorage();
+};
+
+// createTweetLocalStorage
+const createTweetLocalStorage = function (tweet, authorName) {
+  mainTweetFunction(tweet, authorName);
+};
+
 tweetButton.addEventListener('click', function (e) {
   e.preventDefault();
   createTweet(textInput.value, 'Abdallah Alaff');
 });
 
-retweetBtns.forEach(btn => {
-  btn.addEventListener('click', e => {
-    e.preventDefault();
-    // using Event Delegation "closest"
-    const closestParent = btn.closest('.post');
-    const tweetParagraph = closestParent.querySelector('.post-paragraph');
-    const tweetAuthor = closestParent.querySelector('.my_account');
-    createTweet(tweetParagraph.textContent, tweetAuthor.textContent);
-  });
-});
-
-// footer toggle function
-const footerToggle = function (nameIcon) {
-  nameIcon.forEach(icon => {
-    icon.addEventListener('click', function () {
-      icon.classList.toggle('active');
-    });
+const footerToggle = function (iconName) {
+  iconName.addEventListener('click', e => {
+    iconName.classList.toggle('active');
   });
 };
-footerToggle(loveIcon);
-footerToggle(shareIcon);
-footerToggle(repeatIcon);
 
 // local storage
 const setLocalStorage = function () {
   localStorage.setItem('tweets', JSON.stringify(tweets));
 };
 
-// const getLocalStorage = function () {
-//   const data = JSON.parse(localStorage.getItem('tweets'));
-//   tweets = data;
-//   if (!tweets) return;
-//   console.log(tweets);
-// };
+const getLocalStorage = function () {
+  const data = JSON.parse(localStorage.getItem('tweets'));
+  tweets = data;
+  if (!tweets) return;
+  tweets.forEach(oldTweet => {
+    createTweetLocalStorage(oldTweet.tweetText, oldTweet.tweetAuthor);
+  });
+};
+getLocalStorage();
 
-// getLocalStorage();
+// to clear local storage
+const reset = function () {
+  localStorage.removeItem('tweets');
+};
+resetBtn.addEventListener('click', reset);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// for static HTML Posts
+// retweetBtns.forEach(btn => {
+//   btn.addEventListener('click', e => {
+//     e.preventDefault();
+//     // using Event Delegation "closest"
+//     const closestParent = btn.closest('.post');
+//     const tweetParagraph = closestParent.querySelector('.post-paragraph');
+//     const tweetAuthor = closestParent.querySelector('.my_account');
+//     createTweet(tweetParagraph.textContent, tweetAuthor.textContent);
+//   });
+// });
+// // footer toggle function
+// const footerToggleStatic = function (nameIcon) {
+//   nameIcon.forEach(icon => {
+//     icon.addEventListener('click', function () {
+//       icon.classList.toggle('active');
+//     });
+//   });
+// };
+// footerToggleStatic(loveIcon);
+// footerToggleStatic(shareIcon);
+// footerToggleStatic(repeatIcon);
